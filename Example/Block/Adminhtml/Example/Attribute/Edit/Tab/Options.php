7129fbde33efaeedd1314732a1e3fdc1f818c3d8
chandni <17it060@charusat.edu.in>
2021-05-11 00:36:48 +0530

    magento setup

--- Example/Block/Adminhtml/Example/Attribute/Edit/Tab/Options.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Ccc_Example_Block_Adminhtml_Example_Attribute_Edit_Tab_Options extends Mage_Eav_Block_Adminhtml_Attribute_Edit_Options_Abstract
-{
-
-	
-}
--- Example/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Ccc_Example_Helper_Data extends Mage_Core_Helper_Abstract {
-	
-}
