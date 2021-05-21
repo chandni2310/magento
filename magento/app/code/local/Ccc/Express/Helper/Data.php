1dc05dfdaceee0b428558e865846212111e92611
chandni <17it060@charusat.edu.in>
2021-05-21 10:34:13 +0530

    added shipping module

---
+++ magento/app/code/local/Ccc/Express/Helper/Data.php
@@ -0,0 +1,7 @@
+<?php
+
+class Ccc_Express_Helper_Data extends Mage_Core_Helper_Abstract{
+
+}
+
+ ?>
---
+++ magento/app/code/local/Ccc/Express/Model/Demo.php
@@ -0,0 +1,36 @@
+<?php 
+class Ccc_Express_Model_Demo
+extends Mage_Shipping_Model_Carrier_Abstract
+implements Mage_Shipping_Model_Carrier_Interface
+{
+  protected $_code = 'express';
+ 
+  public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+  {
+    $result = Mage::getModel('shipping/rate_result');
+    $result->append($this->_getDefaultRate());
+ 
+    return $result;
+  }
+ 
+  public function getAllowedMethods()
+  {
+    return array(
+      'express' => $this->getConfigData('name'),
+    );
+  }
+ 
+  protected function _getDefaultRate()
+  {
+    $rate = Mage::getModel('shipping/rate_result_method');
+     
+    $rate->setCarrier($this->_code);
+    $rate->setCarrierTitle($this->getConfigData('title'));
+    $rate->setMethod($this->_code);
+    $rate->setMethodTitle($this->getConfigData('name'));
+    $rate->setPrice($this->getConfigData('price'));
+    $rate->setCost(0);
+     
+    return $rate;
+  }
+}
