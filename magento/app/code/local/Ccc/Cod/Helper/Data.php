443a14fc407b23a8575d3960f83d68aa939f5fd1
chandni <17it060@charusat.edu.in>
2021-05-21 10:27:09 +0530

    added payment module

---
+++ magento/app/code/local/Ccc/Cod/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class Ccc_Cod_Helper_Data extends Mage_Core_Helper_Data{
+    
+}
---
+++ magento/app/code/local/Ccc/Cod/Model/Paymentmethod.php
@@ -0,0 +1,5 @@
+<?php 
+class Ccc_Cod_Model_Paymentmethod extends Mage_Payment_Model_Method_Abstract{
+	protected $_code = 'cod';
+}
+?>
